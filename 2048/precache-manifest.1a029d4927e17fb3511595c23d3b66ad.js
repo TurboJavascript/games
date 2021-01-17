cfb46b5f86f3715fa8a3b4914cbc1ccb21698c74
gxkai <30716965+gxkai@users.noreply.github.com>
2021-01-17 20:49:23 +0800

    Updates

--- 2048/precache-manifest.1a029d4927e17fb3511595c23d3b66ad.js
+++ 2048/precache-manifest.1a029d4927e17fb3511595c23d3b66ad.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = [
   {
-    "revision": "ca563993b4294a6bf252",
+    "revision": "015c4d1315d6cd12d52a",
     "url": "css/app.66a7b883.css"
   },
   {
-    "revision": "ca563993b4294a6bf252",
-    "url": "app.0.1.0.1610887590049.js"
+    "revision": "015c4d1315d6cd12d52a",
+    "url": "app.0.1.0.1610887743319.js"
   },
   {
     "revision": "27d53e776f8e93fba8e1",
-    "url": "chunk-vendors.0.1.0.1610887590049.js"
+    "url": "chunk-vendors.0.1.0.1610887743319.js"
   },
   {
-    "revision": "426a72ceecf0817b18be4e646fcecde7",
+    "revision": "5f5b97e8fc2a1843a2dc639c0bd20d13",
     "url": "index.html"
-  },
-  {
-    "revision": "596a8d3e6a63bfccadec9d95f18be61b",
-    "url": "game-img/2048/number.png"
   },
   {
     "revision": "87b801856ca51590d8bfbc1bdca29d54",
     "url": "game-img/2048/number.json"
+  },
+  {
+    "revision": "596a8d3e6a63bfccadec9d95f18be61b",
+    "url": "game-img/2048/number.png"
   },
   {
     "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
--- 2048/service-worker.js
+++ 2048/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.e27dadb97d406604de3918519825e587.js"
+  "precache-manifest.1a029d4927e17fb3511595c23d3b66ad.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "games"});

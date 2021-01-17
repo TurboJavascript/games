0d6a3bf3182d0cb8454e6e4e577692a11ac82d6f
gxkai <30716965+gxkai@users.noreply.github.com>
2021-01-17 19:52:22 +0800

    Updates

---
+++ 2048/precache-manifest.83571306c7a9e3f8cf44bcee671a7be8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "fcda3ab6887e11d2d7e5",
+    "url": "games/2048/css/app.66a7b883.css"
+  },
+  {
+    "revision": "fcda3ab6887e11d2d7e5",
+    "url": "games/2048/js/app.f7979979.js"
+  },
+  {
+    "revision": "edc10fbbc695bf779ee2",
+    "url": "games/2048/js/chunk-vendors.a92402a9.js"
+  },
+  {
+    "revision": "e89767f333048a646a5e6089e4f6a447",
+    "url": "games/2048/index.html"
+  },
+  {
+    "revision": "b04c2d9872be7b5fe4aa6f0bfcf5fc49",
+    "url": "games/2048/game-img/2048/number.json"
+  },
+  {
+    "revision": "596a8d3e6a63bfccadec9d95f18be61b",
+    "url": "games/2048/game-img/2048/number.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "games/2048/robots.txt"
+  }
+];
